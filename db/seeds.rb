4f1e171faed2e842330fc4ee3a115fe1d642e360
jhonsidney <jhon.sidney@gmail.com>
2022-02-27 16:53:17 -0300

    studing about seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://www.comocomprarcriptomoedas.com/wp-content/uploads/2018/02/ETHEREUM-LOGO-2.png"
+)
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png"
+)
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DSH",
+    url_image: "https://thumbs.dreamstime.com/b/%C3%ADcone-de-dashcoin-o-estilo-da-ilustra%C3%A7%C3%A3o-do-vetor-%C3%A9-um-s%C3%ADmbolo-ic%C3%B4nico-liso-com-varia%C3%A7%C3%B5es-azuis-cor-projetado-para-web-e-131604966.jpg"
+)
